dbb4aa728a5f886b30e2b20926a69608456f86e8
TERAfenine <yahkoma@gmail.com>
2025-10-24 16:23:16 +0900

    Detect

--- Assets/Scripts/EnemyStateHandler.cs
+++ Assets/Scripts/EnemyStateHandler.cs
@@ -1,10 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class EnemyStateHandler : MonoBehaviour
 {
-    [SerializeField] private float HP, MaxHp, Damage, Speed, CritRatio, CritPercent;
+    [SerializeField] private float HP, MaxHp, Damage, Speed, CritRatio, CritPercent, Radius;
+    [SerializeField] private Collider2D[] Collider;
+    [SerializeField] private LayerMask layer;
+    void Update()
+    {
+        Collider = Physics2D.OverlapCircleAll(transform.position, Radius, layer);
+        foreach (Collider2D c in Collider)
+        {
+            if (c.CompareTag("Player"))
+            {
+                Debug.Log("Player Detected");
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Radius);
+    }
     public void TakeDamage(float Damage)
     {
         HP -= Damage;
